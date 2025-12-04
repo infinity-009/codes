9d8ed605b882b1008ae2bef5e8ca3b43b3510045
Arjun <reddyvarimallikarjunreddy@gmail.com>
2025-12-04 16:49:42 +0530

    .

---
+++ 226.invert-binary-tree.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode id=226 lang=python3
+#
+# [226] Invert Binary Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+
+        def mirror(left,right):
+            if not root:
+                return 
+            
+            left.left,right.right=right.right,left.left
+
+            left.right,right.left=right.left,left.right
+
+            
+
+
+
+
+    
+
+
+        
+# @lc code=end
+
---
+++ levelorder.py
@@ -0,0 +1,7 @@
+
+
+
+def levelorder(root,height):
+    
+    
+    pass
